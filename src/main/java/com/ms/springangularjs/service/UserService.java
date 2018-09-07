ada51d5c408e016b6213ed2c12e7a4c3b49b5b72
Md Saddam <msg4saddam@gmail.com>
2018-09-07 09:21:41 +0530

    this is first commit

---
+++ src/main/java/com/ms/springangularjs/service/UserService.java
@@ -0,0 +1,25 @@
+package com.ms.springangularjs.service;
+
+
+import com.ms.springangularjs.model.User;
+
+import java.util.List;
+
+public interface UserService {
+	
+	User findById(Long id);
+
+	User findByName(String name);
+
+	void saveUser(User user);
+
+	void updateUser(User user);
+
+	void deleteUserById(Long id);
+
+	void deleteAllUsers();
+
+	List<User> findAllUsers();
+
+	boolean isUserExist(User user);
+}
---
+++ src/main/resources/static/js/app/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('crudApp',['ui.router','ngStorage']);
+
+app.constant('urls', {
+    BASE: 'http://localhost:8081/test',
+    USER_SERVICE_API : 'http://localhost:8081/test/api/user/'
+});
+
+app.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/test/pages/user.html',
+                controller:'UserController',
+                controllerAs:'ctrl',
+                resolve: {
+                    users: function ($q, UserService) {
+                        console.log('Load all users');
+                        var deferred = $q.defer();
+                        UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
+                        return deferred.promise;
+                    }
+                }
+            }).state('test', {
+                url: '/test',
+                templateUrl: '/test/pages/test.html',
+                controller:'UserController',
+                controllerAs:'ctrl',
+                resolve: {
+                    users: function ($q, UserService) {
+                        console.log('Load all users');
+                        var deferred = $q.defer();
+                        UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
+                        return deferred.promise;
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/');
+    }]);
+
